a3a8cb169f6f4ebac65ac855beca781a71bedd7a
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-02-01 09:09:55 +0100

    Added test for getting from the cache

--- test/specs.js
+++ test/specs.js
@@ -1,20 +1,24 @@
-describe('Redis-cache', function() {
+describe('Redis-cache', function () {
 
-  describe('saves', function() {
+  var testUrl = '/testing-the-redis-cache';
+
+  describe('saves', function () {
 
 
     afterEach(function (done) {
-      redis.del(HEADER_PREFIX + '/beer', function (err) {
+      redis.del(HEADER_PREFIX + testUrl, function (err) {
         if (err) {
           return done(err);
         }
-        redis.del(PAYLOAD_PREFIX + '/beer', done);
+        redis.del(PAYLOAD_PREFIX + testUrl, function (err) {
+          done(err);
+        });
       });
     });
 
-    it.only('responseheaders', function(done) {
+    it('responseheaders', function (done) {
       cache.after({
-        url: '/beer'
+        url: testUrl
       }, {
         headers: function () {
           return {
@@ -23,24 +27,52 @@ describe('Redis-cache', function() {
         }
       }, null, null, function (err) {
         if (err) {
-          return done (err);
+          return done(err);
         }
-        redis.get(HEADER_PREFIX + '/beer', function(err, data) {
+        redis.get(HEADER_PREFIX + testUrl, function (err, data) {
+          if (err) {
+            return done(err);
+          }
+          if (!data) {
+            return done('No data!')
+          }
           data.should.equal('{"Accept":"application/xml"}');
           done();
         });
       })
     });
 
-    it('payload', function(done) {
+    it('payload', function (done) {
       cache.after({
-        url: '/beer'
-      })
+        url: testUrl
+      }, {
+        headers: function () {
+          return {};
+        },
+        _data: JSON.stringify({
+          'testing': 1,
+          'expect': 'works'
+        })
+      }, null, null, function (err) {
+        if (err) {
+          return done(err);
+        }
+        redis.get(PAYLOAD_PREFIX + testUrl, function (err, data) {
+          if (err) {
+            return done(err);
+          }
+          if (!data) {
+            return done('No data!')
+          }
+          data.should.equal('{\"testing\":1,\"expect\":\"works\"}');
+          done();
+        });
+      });
     });
   });
 
-  describe('retrieves', function() {
-    it('a response with headers', function() {
+  describe('retrieves', function () {
+    it('a response with headers', function () {
 
     });
   });
