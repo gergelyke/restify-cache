48d9e22595f3db030be1cbd51a0abe92319eeabe
Donnie Armstrong <donaldjarmstrong@gmail.com>
2014-05-05 21:41:00 -0500

    Add support for reading the cache response headers from Restify to set the EXPIRE in Redis
    Additional response header X-Cache to indicate if the response was a cache HIT or MISS.  Tests and documentation are updated.

--- test/specs.js
+++ test/specs.js
@@ -16,61 +16,189 @@ describe('Redis-cache', function () {
       });
     });
 
-    it('responseheaders', function (done) {
-      cache.after({
-        url: testUrl
-      }, {
-        headers: function () {
-          return {
-            'Accept': 'application/xml'
-          };
-        }
-      }, null, null, function (err) {
-        if (err) {
-          return done(err);
-        }
-        redis.get(HEADER_PREFIX + testUrl, function (err, data) {
-          if (err) {
-            return done(err);
-          }
-          if (!data) {
-            return done('No data!')
-          }
-          data.should.equal('{"Accept":"application/xml"}');
-          done();
-        });
-      })
-    });
+      describe("cache-control not specified", function () {
+          it('responseheaders', function (done) {
+              cache.after({
+                  url: testUrl
+              }, {
+                  headers: function () {
+                      return {
+                          'Accept': 'application/xml'
+                      };
+                  },
+                  header: function(key, value) {
+                      key.should.equal('X-Cache');
+                      value.should.equal('MISS');
+                  },
+                  getHeader: function () {
+                      return undefined;
+                  }
+              }, null, null, function (err) {
+                  if (err) {
+                      return done(err);
+                  }
+                  redis.get(HEADER_PREFIX + testUrl, function (err, data) {
+                      if (err) {
+                          return done(err);
+                      }
+                      if (!data) {
+                          return done('No data!')
+                      }
+                      data.should.equal('{"Accept":"application/xml"}');
 
-    it('payload', function (done) {
-      cache.after({
-        url: testUrl
-      }, {
-        headers: function () {
-          return {};
-        },
-        _data: JSON.stringify({
-          'testing': 1,
-          'expect': 'works'
-        })
-      }, null, null, function (err) {
-        if (err) {
-          return done(err);
-        }
-        redis.get(PAYLOAD_PREFIX + testUrl, function (err, data) {
-          if (err) {
-            return done(err);
-          }
-          if (!data) {
-            return done('No data!')
-          }
-          data.should.equal('{\"testing\":1,\"expect\":\"works\"}');
-          done();
-        });
+                      redis.ttl(HEADER_PREFIX + testUrl, function (err, data) {
+                          if (err) {
+                              return done(err);
+                          }
+                          if (!data) {
+                              return done('No data!')
+                          }
+                          data.should.equal(3600);
+
+                          done();
+                      });
+                  });
+              })
+          });
+
+          it('payload', function (done) {
+              cache.after({
+                  url: testUrl
+              }, {
+                  headers: function () {
+                      return {};
+                  },
+                  header: function(key, value) {
+                      key.should.equal('X-Cache');
+                      value.should.equal('MISS');
+                  },
+                  getHeader: function () {
+                      return undefined;
+                  },
+                  _data: JSON.stringify({
+                      'testing': 1,
+                      'expect': 'works'
+                  })
+              }, null, null, function (err) {
+                  if (err) {
+                      return done(err);
+                  }
+                  redis.get(PAYLOAD_PREFIX + testUrl, function (err, data) {
+                      if (err) {
+                          return done(err);
+                      }
+                      if (!data) {
+                          return done('No data!')
+                      }
+                      data.should.equal('{\"testing\":1,\"expect\":\"works\"}');
+
+                      redis.ttl(PAYLOAD_PREFIX + testUrl, function (err, data) {
+                          if (err) {
+                              return done(err);
+                          }
+                          if (!data) {
+                              return done('No data!')
+                          }
+                          data.should.equal(3600);
+                          done();
+                      });
+                  });
+              });
+          });
       });
-    });
-  });
 
+      describe("cache-control specified", function () {
+          it('responseheaders', function (done) {
+              cache.after({
+                  url: testUrl
+              }, {
+                  headers: function () {
+                      return {
+                          'Accept': 'application/xml',
+                          'Cache-Control': 'public, max-age=54321'
+                      };
+                  },
+                  header: function(key, value) {
+                      key.should.equal('X-Cache');
+                      value.should.equal('MISS');
+                  },
+                  getHeader: function () {
+                      return 'public, max-age=54321';
+                  }
+              }, null, null, function (err) {
+                  if (err) {
+                      return done(err);
+                  }
+                  redis.get(HEADER_PREFIX + testUrl, function (err, data) {
+                      if (err) {
+                          return done(err);
+                      }
+                      if (!data) {
+                          return done('No data!')
+                      }
+                      data.should.equal('{"Accept":"application/xml","Cache-Control":"public, max-age=54321"}');
+
+                      redis.ttl(HEADER_PREFIX + testUrl, function (err, data) {
+                          if (err) {
+                              return done(err);
+                          }
+                          if (!data) {
+                              return done('No data!')
+                          }
+                          data.should.equal(54321);
+                      });
+                      done();
+                  });
+              })
+          });
+
+          it('payload', function (done) {
+              cache.after({
+                  url: testUrl
+              }, {
+                  headers: function () {
+                      return {};
+                  },
+                  header: function(key, value) {
+                      key.should.equal('X-Cache');
+                      value.should.equal('MISS');
+                  },
+                  getHeader: function () {
+                      return 'public, max-age=1';
+                  },
+                  _data: JSON.stringify({
+                      'testing': 1,
+                      'expect': 'works'
+                  })
+              }, null, null, function (err) {
+                  if (err) {
+                      return done(err);
+                  }
+                  redis.get(PAYLOAD_PREFIX + testUrl, function (err, data) {
+                      if (err) {
+                          return done(err);
+                      }
+                      if (!data) {
+                          return done('No data!')
+                      }
+                      data.should.equal('{\"testing\":1,\"expect\":\"works\"}');
+
+                      redis.ttl(PAYLOAD_PREFIX + testUrl, function (err, data) {
+                          if (err) {
+                              return done(err);
+                          }
+                          if (!data) {
+                              return done('No data!')
+                          }
+                          data.should.equal(1);
+                      });
+
+                      done();
+                  });
+              });
+          });
+      });
+  });
   describe('retrieves', function () {
 
     beforeEach(function (done) {
@@ -81,6 +209,11 @@ describe('Redis-cache', function () {
           return {
             'Accept': 'application/xml'
           };
+        },
+        header: function() {
+        },
+        getHeader: function() {
+            return undefined;
         },
         _data: JSON.stringify({
           'testing': 1,
@@ -101,14 +234,20 @@ describe('Redis-cache', function () {
     });
 
     it('a response', function (done) {
+        var headers = {};
       cache.before({
         url: testUrl
       }, {
+        header: function(key, value) {
+            headers[key] = value;
+        },
         end: function (data) {
           data.should.equal('{\"testing\":1,\"expect\":\"works\"}');
+
+          headers['X-Cache'].should.equal('HIT');
+          headers['Accept'].should.equal('application/xml');
           done();
         },
-        setHeader: function() {},
         writeHead: function() {}
       }, function(){
 
